78a2d8d3d354c0443893cfd59c0ba33414aa8236
Anthony Southavilay <anthony.southavilay@gmail.com>
2019-12-10 11:07:39 -0500

    Started Tests for Location DAO

---
+++ final-capstone/vue_starter_dotnet/backend/SampleApi.Tests/DAL/test_setup.sql
@@ -0,0 +1,20 @@
+﻿-- Remove all the data from the DB
+
+DELETE FROM Location;
+
+-- insert some test data
+
+-- Location
+INSERT INTO Location (
+	   [Id]
+      ,[Name]
+      ,[ShortDesc]
+      ,[LongDesc]
+      ,[Latitude]
+      ,[Longitude]
+      ,[Address]
+      ,[Image]
+      ,[PhoneNum]
+      ,[PriceLevel]
+      ,[WebsiteURL])
+	  VALUES (1 , 'Test Location', 'Test Short Description', 'Test Long Description', 12.123456, -98,765432, '1 Main Street Rd', '', 1, 'https://www.test.com');
