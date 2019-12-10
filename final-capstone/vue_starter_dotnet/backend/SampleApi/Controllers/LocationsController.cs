fe2565e72ebc687117a39f580abf80ea9b5e8c92
Brandon Best <bestb23@yahoo.com>
2019-12-10 10:07:47 -0500

    wrote location api controller

---
+++ final-capstone/vue_starter_dotnet/backend/SampleApi/Controllers/LocationsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SampleApi.DAL;
+using SampleApi.Models;
+
+namespace SampleApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationsController : Controller
+    {
+        private LocationSqlDAO dao;
+        public LocationsController(LocationSqlDAO dao)
+        {
+            this.dao = dao;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Location>> GetLocations()
+        {
+            List<Location> locations = dao.GetAllLocations();
+
+            return Ok(locations);
+        }
+
+        [HttpGet("{id}", Name = "GetLocationById")]
+        public ActionResult<Location> GetLocationById(int id)
+        {
+            Location location = dao.GetLocation(id);
+
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(location);
+        }
+    }
+}
