bb03edfb800c732d977597ad64d5ffc53ad78b12
Brandon Best <bestb23@yahoo.com>
2019-12-11 16:30:55 -0500

    added fetch user location function to listlocations.vue which is our test file

--- final-capstone/vue_starter_dotnet/frontend/src/components/ListLocations.vue
+++ final-capstone/vue_starter_dotnet/frontend/src/components/ListLocations.vue
@@ -1,5 +1,7 @@
 <template>
 <div>
+    <p>location lat: {{userLocation.coords.latitude}}</p>
+    <p>location long: {{userLocation.coords.longitude}}</p>
     <ul>
         <li v-for="location in locationsList" v-bind:key="location.name">
             <p>{{location.name}}</p>
@@ -14,13 +16,26 @@ export default {
     name: 'list-locations',
     data(){
         return {
-            locationsList: []
+            locationsList: [],
+            userLocation: null
         }
     },
     created() {
-        this.fetchLocations()
+        this.fetchLocations();
+
+        this.fetchUserLocation();
+
+        this.directionsTest();
     },
     methods: {
+        fetchUserLocation(){
+            navigator.geolocation.getCurrentPosition(pos => {
+                this.userLocation = pos;
+                console.log(this.userLocation);
+            }, err => {
+                this.errorStr = err.message;
+            })
+        },
         fetchLocations(){
             fetch(`${process.env.VUE_APP_REMOTE_API}/locations`).then(
                 (resp) => {
@@ -38,6 +53,26 @@ export default {
             ).catch(
                 (err) => {
                 console.log(err);
+                }
+            )
+        },
+        directionsTest(){
+            fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=41.4973,-81.6933&destination=41.500670,-81.680910&mode=walking&units=imperial&key=AIzaSyA8qsiApLrQeAAIrs-Rx8lGQHOxa_lxHc0`).then(
+                (resp) => {
+                    if (resp.ok) {
+                        resp.json().then(
+                            (data) => {
+                                this.directions = data;
+                                console.log(this.directions);
+                            }
+                        )
+                    }else {
+                        console.log(`Error: ${resp.status} ${resp.statusText}`)
+                    }
+                }
+            ).catch(
+                (err) => {
+                    console.log(err);
                 }
             )
         }
