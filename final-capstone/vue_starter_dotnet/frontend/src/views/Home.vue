aa64af676e747ee0569f20d77ffd76006ca4cd79
Robert Holland <robertrhollandiii@gmail.com>
2019-12-12 11:30:03 -0500

    Home page card

--- final-capstone/vue_starter_dotnet/frontend/src/views/Home.vue
+++ final-capstone/vue_starter_dotnet/frontend/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="flex-container">
     <div class="message-box">
       <h1>Hello User!</h1>
-      <h2>We have so many places to explore today!</h2>
+      <h3>We have so many places to explore today!</h3>
       <p>Click on "Nearby Locations" or enter a location in the search bar above to get started.</p>
     </div>
     <div class="nearby-button"><img src="@/assets/gpsButton.png"/></div>
@@ -22,7 +22,13 @@ export default {
     background-color: white;
     width: 40%;
     margin: auto;
-    text-align: 
+    text-align: center;
+    padding: 5px;
+    box-shadow: 7px 7px 15px 0px  rgba(0,0,0,0.3);
+  }
+
+  .message-box > h1 {
+    color: #607c85;
   }
 
 </style>
