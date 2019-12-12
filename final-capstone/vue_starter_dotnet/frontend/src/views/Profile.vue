924cf394ae533daa2d07c629f29118eb93af2194
Robert Holland <robertrhollandiii@gmail.com>
2019-12-12 15:10:41 -0500

    Placeholder Profile vue ready

--- final-capstone/vue_starter_dotnet/frontend/src/views/Profile.vue
+++ final-capstone/vue_starter_dotnet/frontend/src/views/Profile.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="flex-container">
-    <div class="message-box">
-      <h1>Hello User!</h1>
-      <h3>We have so many places to explore today!</h3>
-      <p>Click on "Nearby Locations" or enter a location in the search bar above to get started.</p>
+  <div class="grid-container">
+    <div class="portrait">
+        <img src="@/assets/user.png">
+        <h3>Samantha</h3>
     </div>
-    <div class="nearby-button"><router-link :to="{ name: 'NearbyLocations' }" exact><img src="@/assets/gpsButton.png"/><h2 id="nearby-location-text">NEARBY LOCATIONS</h2></router-link></div>
+    <div class="history">
+        <p>Placeholder History</p>
+    </div>
+    <div class="badges">
+        <p>Placeholder Badge</p>
+    </div>
   </div>
 </template>
 
@@ -17,6 +21,46 @@ export default {
 
 
 <style scoped>
+
+.grid-container {
+    display: grid;
+    grid-column-gap: 10px;
+    grid-row-gap: 10px;
+    grid-template-columns: 1fr 2fr 2fr 1fr;
+    grid-template-rows: 1fr 3fr;
+    grid-template-areas: 
+        '. portrait portrait .'
+        '. history badges .';
+}
+
+.portrait {
+    background-color: white;
+    border-radius: 15px;
+    grid-area: portrait;
+    text-align: center;
+    padding: 5px;
+    box-shadow: 7px 7px 15px 0px  rgba(0,0,0,0.3);
+}
+
+.history {
+    background-color: white;
+    border-radius: 15px;
+    grid-area: history;
+    text-align: center;
+    box-shadow: 7px 7px 15px 0px  rgba(0,0,0,0.3);
+}
+
+.badges {
+    background-color: white;
+    border-radius: 15px;
+    grid-area: badges;
+    text-align: center;
+    box-shadow: 7px 7px 15px 0px  rgba(0,0,0,0.3);
+}
+
+.portrait > img {
+    width: 20%;
+}
 
 
 </style>
