b7b17013348ad9a12ba8c5dfaa0f6e71c32b2489
Robert Holland <robertrhollandiii@gmail.com>
2019-12-12 13:34:14 -0500

    Added routing for Profile and added Profile.vue

--- final-capstone/vue_starter_dotnet/frontend/src/router.js
+++ final-capstone/vue_starter_dotnet/frontend/src/router.js
@@ -5,6 +5,7 @@ import Home from './views/Home.vue'
 import Login from './views/Login.vue'
 import Register from './views/Register.vue'
 import NearbyLocations from './views/NearbyLocations.vue'
+import Profile from './views/Profile.vue'
 
 Vue.use(Router)
 
@@ -51,6 +52,14 @@ const router = new Router({
       component: NearbyLocations,
       meta: {
         requiresAuth: false
+      }
+    },
+    {
+      path: "/profile",
+      name: "profile",
+      component: Profile,
+      meta: {
+        requiresAuth: true
       }
     },
   ]
---
+++ final-capstone/vue_starter_dotnet/frontend/src/views/Profile.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex-container">
+    <div class="message-box">
+      <h1>Hello User!</h1>
+      <h3>We have so many places to explore today!</h3>
+      <p>Click on "Nearby Locations" or enter a location in the search bar above to get started.</p>
+    </div>
+    <div class="nearby-button"><router-link :to="{ name: 'NearbyLocations' }" exact><img src="@/assets/gpsButton.png"/><h2 id="nearby-location-text">NEARBY LOCATIONS</h2></router-link></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'profile'
+}
+</script>
+
+
+<style scoped>
+
+
+</style>
+
