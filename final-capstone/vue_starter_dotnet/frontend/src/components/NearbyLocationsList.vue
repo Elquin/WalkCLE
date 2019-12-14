691ce7d5b9ab0eb7bac33737bae3f200793f51ac
Brandon Best <bestb23@yahoo.com>
2019-12-14 12:50:11 -0500

    formatted nearbysearch and created link to deatails page

--- final-capstone/vue_starter_dotnet/frontend/src/components/NearbyLocationsList.vue
+++ final-capstone/vue_starter_dotnet/frontend/src/components/NearbyLocationsList.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="flex-container">
-    <div class="flex-item" v-for="location in closestLocationsList" v-bind:key="location.name">
-
-      <div class="location-image"><router-link v-bind:to="{name:'Details', params: {id: location.id}}"><img src="@/assets/location-image.jpg"/></router-link></div>
+      <router-link class="flex-item" v-for="location in closestLocationsList" v-bind:key="location.name" tag="div" v-bind:to="{name:'LocationDetails', params: {id: location.id}}">
+      <div class="location-image"><img src="@/assets/location-image.jpg"/></div>
           <ul>
-              <li>
-                  <router-link v-bind:to="{name:'Details', params: {id: location.id}}">
-                  <h3>{{location.name}}</h3></router-link>
+           
+              <li >
+                  
+                  <h3>{{location.name}}</h3>
                   <p>{{location.shortDescription}}</p>
                   <p>{{location.address}}</p>
+                  
               </li>
+             
           </ul>
-    </div>
+          </router-link>
   </div>
 </template>
 
@@ -184,6 +186,7 @@ export default {
       width: 40%;
       display: flex;
       box-shadow: 7px 7px 15px 0px  rgba(0,0,0,0.3);
+      cursor: pointer;
     }
 
     .flex-item:hover {
@@ -209,6 +212,11 @@ export default {
     }
 
 
+    a {  text-decoration: none; color: black;}
+
+    a:visited {  text-decoration: none; color: black;}
+
+    
 
     /* div {
       margin: 20px;
