08141a3b302c05822f5a3af1332a783044eac640
Robert Holland <robertrhollandiii@gmail.com>
2019-12-14 18:12:51 -0500

    Styling for location details page mostly done. Confirm that Money rating is good. Hyperlinks may have broke. Have to investigate. TODO: add images.

--- final-capstone/vue_starter_dotnet/frontend/src/components/DetailsPage.vue
+++ final-capstone/vue_starter_dotnet/frontend/src/components/DetailsPage.vue
@@ -1,18 +1,22 @@
 <template>
-<div>
-    <gmap-map id="map"
-    :center="center" 
-    :zoom="13"
-    >
-    
-    <gmap-marker v-for="m in markers" v-bind:key="m.position" :position="m.position"/>
-    </gmap-map>
-    <p>Name: {{location.name}}</p>
-    <p>Description: {{location.longDescription}}</p>
-    <p>Address: {{location.address}}</p>
-    <p>Phone Number: {{location.phoneNumber}}</p>
-    <p>Website: {{location.websiteURL}}</p>
-    <p>Price Level: {{location.priceLevel}}</p>
+  <div class="grid-container">
+    <div class="map-container">
+      <gmap-map id="map" :center="center" :zoom="13">
+            <gmap-marker v-for="m in markers" v-bind:key="m.position" :position="m.position"/>
+      </gmap-map>
+    </div>
+    <div class="details-card">
+        <div class="location-title"><h3>{{location.name}}</h3></div>
+            <div class="details-content">
+                <ul>
+                    <li class="description">{{location.longDescription}}</li>
+                    <li><img class="icon" src="@/assets/icons/marker.png"/> {{location.address}}</li>
+                    <li><img class="icon" src="@/assets/icons/phone.png"/> {{location.phoneNumber}}</li>
+                    <li><img class="icon" src="@/assets/icons/globe.png"/> <a :href="location.websiteURL">{{location.websiteURL}}</a></li>
+                    <li><img id="money" v-for="n in location.priceLevel" v-bind:key="n.priceLevel" src="@/assets/money.png"/></li>
+                </ul>
+            </div>
+    </div>
   </div>
 </template>
 
@@ -71,10 +75,66 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 #map {
     width:300px;  
     height: 300px;
+}
+
+.grid-container {
+    
+    display: grid;
+    grid-column-gap: 10px;
+    grid-row-gap: 10px;
+    grid-template-columns: 1fr 2fr 2fr 1fr;
+    grid-template-rows: 4fr 3fr;
+    grid-template-areas: 
+        '. map map .'
+        '. details details .';
+        
+}
+
+
+.map-container {
+    z-index: -50;
+    background-color: white;
+    border-radius: 10px;
+    grid-area: map;
+    text-align: center;
+    box-shadow: 7px 7px 15px 0px  rgba(0,0,0,0.3);
+}
+
+#map {
+    width: 100%;
+    height: 100%;
+}
+
+.details-card {
+    z-index: -50;
+    background-color: white;
+    border-radius: 10px;
+    grid-area: details;
+    text-align: center;
+    box-shadow: 7px 7px 15px 0px  rgba(0,0,0,0.3);
+    opacity: 95%;
+}
+
+.details-content ul {
+    list-style-type: none;
+    padding-left: 0px;
+}
+
+.description {
+  margin-bottom: 15px;
+}
+
+.icon {
+  width: 20px;
+}
+
+#money {
+  width: 30px;
+  margin-top: 5px;
 }
 
 </style>
