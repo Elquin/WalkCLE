7f4c44f4ce7ea39802ffae6c9432c54829ae1f7e
Kit Partezana <kit.partezana@gmail.com>
2019-12-16 16:42:06 -0500

    Attempting to search on category

--- final-capstone/vue_starter_dotnet/frontend/src/components/NearbyLocationsList.vue
+++ final-capstone/vue_starter_dotnet/frontend/src/components/NearbyLocationsList.vue
@@ -39,9 +39,10 @@ export default {
             const filter = new RegExp(this.search,'i');
             let arr1 = this.closestLocationsList.filter(location => location.name.match(filter));
             let arr2 = this.closestLocationsList.filter(location => location.longDescription.match(filter));
-            arr1.concat(arr2);
-            // let arr3 = this.closestLocationsList.filter(location => location.category.filter(cat => cat.filter.match(filter)));
-            // arr1.concat(arr3);
+            arr1 = arr1.concat(arr2);
+            // let arr3 = this.closestLocationsList.filter(location => location.category.foreach(cat => cat.match(filter)));
+            // let arr3 = this.closestLocationsList.filter(location => location.category.filter(cat => cat.match(filter)));
+            // arr1 = arr1.concat(arr3);
             return Array.from(new Set(arr1));
       }
     },
