b13dee7a904bcc799be57a62654c96cd3cd4255f
Kit Partezana <kit.partezana@gmail.com>
2019-12-17 11:01:41 -0500

    Trying category search inside location

--- final-capstone/vue_starter_dotnet/frontend/src/components/NearbyLocationsList.vue
+++ final-capstone/vue_starter_dotnet/frontend/src/components/NearbyLocationsList.vue
@@ -39,6 +39,8 @@ export default {
             let arr1 = this.closestLocationsList.filter(location => location.name.match(filter));
             let arr2 = this.closestLocationsList.filter(location => location.longDescription.match(filter));
             arr1 = arr1.concat(arr2);
+            // let arr3 =  this.closestLocationsList.filter(function(location) {return location.category.some(function(cat) {return cat.toLowerCase().contains(this.search.toLowerCase()); }); });
+            // let arr3 =  this.closestLocationsList.filter(function(location) {return location.category.some(function(cat) {return cat.includes(this.search); }); });
             // let arr3 = this.closestLocationsList.filter(location => location.category.foreach(cat => cat.match(filter)));
             // let arr3 = this.closestLocationsList.filter(location => location.category.filter(cat => cat.match(filter)));
             // arr1 = arr1.concat(arr3);
