6d8e6149928ab8f036f7ee80562851887446cb00
Robert Holland <robertrhollandiii@gmail.com>
2019-12-17 16:52:47 -0500

    Nearby Images merge conflict because of 'require' Added it back

--- final-capstone/vue_starter_dotnet/frontend/src/components/NearbyLocationsList.vue
+++ final-capstone/vue_starter_dotnet/frontend/src/components/NearbyLocationsList.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="flex-container">
       <router-link class="flex-item" v-for="location in filteredLocations" v-bind:key="location.name" tag="div" v-bind:to="{name:'LocationDetails', params: {id: location.id}}">
-<<<<<<< HEAD
-      <div class="location-image"><img :src="getImgUrl(location.id)"/></div>
-=======
       <div class="location-image"><img :src="require('../assets/locations/' + location.id + '.jpg')"/></div>
->>>>>>> e4747cc76382b2272acee6034a30d20e9bbbf9fa
           <ul>
            
               <li >
