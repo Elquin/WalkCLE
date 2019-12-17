c6d3af5262395c72758e25bc421653b9455c75ea
Brandon Best <bestb23@yahoo.com>
2019-12-17 18:37:14 -0500

    forgot to save badges controller

--- final-capstone/vue_starter_dotnet/backend/SampleApi/Controllers/BadgeController.cs
+++ final-capstone/vue_starter_dotnet/backend/SampleApi/Controllers/BadgeController.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SampleApi.DAL;
 
 namespace SampleApi.Controllers
 {
+    [Route("api/[controller]")]
     public class BadgeController : Controller
     {
-        public IActionResult Index()
+        private IUserDAO userDao;
+        private IBadgeDAO badgeDao;
+        public BadgeController(IUserDAO userDao, IBadgeDAO badgeDao)
         {
-            return View();
+            this.userDao = userDao;
+            this.badgeDao = badgeDao;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetUserBadges()
+        {
+            int userId = GetCurrentUserId();
+            List<string> result = badgesDao.GetBadges(userId);
+            return Ok(result);
+        }
+
+        private int GetCurrentUserId()
+        {
+            return userDao.GetUser(base.User.Identity.Name).Id;
         }
     }
 }
